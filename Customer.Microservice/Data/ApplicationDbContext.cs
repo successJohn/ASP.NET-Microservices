3c79a2f78142b8ccd9afe6f54284eca2e661d651
Success John <successojohn@gmail.com>
2022-04-24 15:02:11 +0200

    Microservices

---
+++ Customer.Microservice/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Customer.Microservice.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Customer.Microservice.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            :base(options)
+        {
+
+        }
+
+       public DbSet<Customers> Customerss { get; set; }
+
+    }
+}
---
+++ Product.Microservice/Entities/Products.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Product.Microservice.Entities
+{
+    public class Products
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Rate { get; set; }
+    }
+}
